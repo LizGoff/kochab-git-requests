f13c4d3d531cdf572c157d48bd7885b1dd0db72f
Liz Goff <livingearthcreations@gmail.com>
2018-04-18 15:52:49 -0500

    added messages under quotes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,29 +6,43 @@ console.log('JS');
 function onReady() {
     console.log('jquery loaded');
     $.ajax({
-        type: 'GET', 
+        type: 'GET',
         url: '/all-quotes'
     })
-    .then(function(response){
-        console.log(response);
-    });
+        .then(function (response) {
+            console.log(response);
+        });
 
     $('#newQuoteButton').on('click', changeQuote);
-
+    listQuotes();
 
 }
 
 function changeQuote() {
     $.ajax({
-        type: 'GET', 
+        type: 'GET',
         url: '/quote'
     })
-    .then(function(response){
-        console.log(response);
-        $('#displayQuote').text(`${response.quote} - ${response.author}`);
-    });
+        .then(function (response) {
+            console.log(response);
+            $('#displayQuote').text(`${response.quote} - ${response.author}`);
+        });
 
 }
 
+function listQuotes() {
+    $.ajax({
+        type: 'GET',
+        url: '/all-quotes'
+    })
+        .then(function (response) {
+            console.log(response);
+            response.forEach(object){
+                $('#listOfQuotes').append(`<p>${object.quote} - ${object.author}</p>`);
+            };
+        });
+}
+
+// "listOfQuotes"
 
 // module.exports = client;
