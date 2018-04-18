b4b5adf90f65531fea878f6f533cfaf6a1339ced
Liz Goff <livingearthcreations@gmail.com>
2018-04-18 14:29:03 -0500

    all files conected in dom

---
+++ Server/public/client.js
@@ -0,0 +1,11 @@
+console.log('client side up');
+
+$(document).ready(onReady);
+console.log('JS');
+
+function onReady() {
+    console.log('jquery loaded');
+}
+
+
+// module.exports = client;
---
+++ Server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+// tie to files 
+
+const quotes_data = require('./modules/quotes-data');
+
+// const style = require('./public/style.css');
+
+// const client = require('./public/client.js');
+
+// const index = require('./public/index.html');
+
+app.use(express.static('server/public'));
+
+app.get('/quote', (req, res) => {
+   const randomNumber =  Math.floor(Math.random() * quotes_data.length);
+    res.send(quotes_data[randomNumber]);
+});
+
+app.get('/all-quotes', (req, res) => {
+    res.send(quotes_data);
+});
+
+// app.get('/', (req, res) => {
+//     // console.log('yep it worked!');
+//     res.send('happy wednesday');
+// });
+
+// anonymous call back function
+app.listen(PORT, () => {
+    console.log(`Running on port: ${PORT}`);
+});
